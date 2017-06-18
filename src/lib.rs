c46fc35a1400d9a4bbb75782f47616ed36d84d1a
Cody P Schafer <dev@codyps.com>
2017-06-18 18:33:35 -0400

    docs

--- src/lib.rs
+++ src/lib.rs
@@ -65,6 +65,34 @@ macro_rules! index_fixed {
     } };
 }
 
+/**
+ * `slice::get` and `slice::get_mut`, but return an `Option<&[T;N]>` or `Option<&mut [T;N]>`
+ *
+ * Will not compile if bounds are not static.
+ *
+ * Will not compile if end bound proceeds start bound.
+ *
+ * # Format
+ *
+ * ```notest
+ * index_fixed_get! ( {&,&mut} <slice> ; .. <end>)
+ * index_fixed_get! ( {&,&mut} <slice> ; <start> , .. <end>)
+ * index_fixed_get! ( {&,&mut} <slice> ; <start> , ... <end>)
+ * ```
+ *
+ * # Examples
+ *
+ * ```
+ * #[macro_use]
+ * extern crate index_fixed;
+ *
+ * fn main() {
+ *   let my_slice = [1, 2, 3, 4];
+ *   let slice_of_2 = index_fixed_get!(&my_slice ; .. 2);
+ *   assert_eq!(slice_of_2, Some(&[1,2]));
+ * }
+ * ```
+ */
 #[macro_export]
 macro_rules! index_fixed_get {
     (&mut $s:expr ;  .. $e:expr) => {
