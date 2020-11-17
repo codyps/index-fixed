88e2851f10c02c164a705a59e4fb8e5b3c425154
Cody P Schafer <dev@codyps.com>
2020-11-16 21:45:08 -0500

    fmt

--- tests/get.rs
+++ tests/get.rs
@@ -3,23 +3,23 @@ extern crate index_fixed;
 
 #[test]
 fn const_to() {
-    let a = [1u8,2,3,6];
+    let a = [1u8, 2, 3, 6];
     {
-        let b : Option<&[u8;1]> = index_fixed_get!(&a; ..1);
+        let b: Option<&[u8; 1]> = index_fixed_get!(&a; ..1);
         assert_eq!(b, Some(&[1]));
     }
 
     {
-        let b : Option<&[u8;2]> = index_fixed_get!(&a; ..2);
+        let b: Option<&[u8; 2]> = index_fixed_get!(&a; ..2);
         assert_eq!(b, Some(&[1, 2]));
     }
 }
 
 #[test]
 fn mut_to() {
-    let mut a = [1u8,2,3,6];
+    let mut a = [1u8, 2, 3, 6];
     {
-        let b : Option<&mut [u8;2]> = index_fixed_get!(&mut a; ..2);
+        let b: Option<&mut [u8; 2]> = index_fixed_get!(&mut a; ..2);
         assert_eq!(b, Some(&mut [1, 2]));
 
         let b = b.unwrap();
@@ -31,18 +31,18 @@ fn mut_to() {
 
 #[test]
 fn const_range() {
-    let a = [1u8,2,3,6];
+    let a = [1u8, 2, 3, 6];
     {
-        let b : Option<&[u8;2]> = index_fixed_get!(&a; 1 * 2, .. 6 - 2);
+        let b: Option<&[u8; 2]> = index_fixed_get!(&a; 1 * 2, .. 6 - 2);
         assert_eq!(b, Some(&[3, 6]));
     }
 }
 
 #[test]
 fn mut_range() {
-    let mut a = [1u8,2,3,6];
+    let mut a = [1u8, 2, 3, 6];
     {
-        let b : Option<&mut [u8;2]> = index_fixed_get!(&mut a; 4/2, .. 2 + 2);
+        let b: Option<&mut [u8; 2]> = index_fixed_get!(&mut a; 4/2, .. 2 + 2);
         assert_eq!(b, Some(&mut [3, 6]));
 
         b.unwrap()[0] = 5;
@@ -59,13 +59,13 @@ fn type_infer() {
 
 #[test]
 fn zero_len_to_one() {
-    let a : [u8;0] = [];
+    let a: [u8; 0] = [];
     assert_eq!(index_fixed_get!(&a; ..1), None);
 }
 
 #[test]
 fn zero_to_zero() {
-    let a : [u8;0] = [];
+    let a: [u8; 0] = [];
     assert_eq!(index_fixed_get!(&a; ..0), Some(&[]))
 }
 
@@ -79,5 +79,5 @@ fn out_of_range() {
 fn const_usage() {
     const X: usize = 2;
     let a = [1u8, 2, 4, 5];
-    assert_eq!(index_fixed_get!(&a; X, .. X * 2), Some(&[4,5]));
+    assert_eq!(index_fixed_get!(&a; X, .. X * 2), Some(&[4, 5]));
 }
